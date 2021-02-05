e021e4a7c01d24300d83d99016b2b076455a792d
avocadoroll <78227514+avocadoroll@users.noreply.github.com>
2021-02-04 19:53:05 -0700

    Create iframe-video.js

---
+++ iframe-video.js
@@ -0,0 +1,9 @@
+<script>
+    function prepareFrame() {
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", "https://www.youtube.com/embed/aWvVNDiW6oA");
+        ifrm.style.width = "640px";
+        ifrm.style.height = "480px";
+        document.body.appendChild(ifrm);
+    }
+</script> 
